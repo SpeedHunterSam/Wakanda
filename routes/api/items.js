c5c41b14c92a29cd863d7ff378427f3e7c9f1914
Brian <brian.s2@gmail.com>
2019-10-22 12:56:19 -0500

    delete route completed

--- routes/api/items.js
+++ routes/api/items.js
@@ -14,13 +14,23 @@ router.get("/", (req, res) => {
 });
 
 // post request to api/items
-//gets all items
+//create an item
 
 router.post("/", (req, res) => {
     const newItem = new Item({
         name: req.body.name
     });
     newItem.save().then(item=> res.json(item));
+});
+
+// @ route DELETE api/items/:id
+//Delete a Database Item
+//d
+
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+  .then(item => item.remove().then(()=> res.json({success: true})))
+  .catch(err => res.status(404).json({success: false}));
 });
 
 
