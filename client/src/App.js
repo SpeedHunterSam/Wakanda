4ecb7b480e301f6fcb61e2d9372c788252306a30
Brian <brian.s2@gmail.com>
2019-10-22 14:32:18 -0500

    reactnavbar up and running

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import AppNavbar from "./components/appNavbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-      <h1>Hello</h1>
+      <AppNavbar />
     </div>
     );
   }
