9675489c962b69a0c6193a94e47488b8786b5d9d
Brian <brian.s2@gmail.com>
2019-10-22 11:20:15 -0500

    getting express and mongoose set up

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//Bodyparser MiddleWare
+
+app.use(bodyParser.json());
+
