4b8b332b673befa348774f9844a1c4dfe9e8a22d
Brian <brian.s2@gmail.com>
2019-10-23 09:06:50 -0500

    added axios and fixed localhost to 127.0.0.1 issue in proxy field of client package.json

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,9 +1,15 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM} from "./types";
+import axios from "axios";
 
-export const getItems = () => {
-    return {
-        type: GET_ITEMS
-    };
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING} from "./types";
+
+export const getItems = () => dispatch => {
+
+    dispatch(setItemsLoading());
+    axios.get("/api/items").then(res=> 
+            dispatch({
+                type: GET_ITEMS,
+                payload: res.data
+            }))
 };
 
 export const deleteItem = (id) => {
@@ -19,3 +25,9 @@ export const addItem = item => {
         payload: item
     };
 };
+
+export const setItemsLoading = () => {
+    return {
+        type: ITEMS_LOADING
+    }
+}
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,13 +1,10 @@
 import uuid from "uuid";
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM} from "../actions/types";
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING} from "../actions/types";
 
 const initialState = {
-    items: [
-        { id: uuid(), name: "Eggs" },
-        { id: uuid(), name: "Milk" },
-        { id: uuid(), name: "Steak" },
-        { id: uuid(), name: "Candy" }
-    ]
+    //the array of items for the list
+    items: [ ],
+    loading: false
 };
 
 export default function( state = initialState, action){
@@ -15,7 +12,9 @@ export default function( state = initialState, action){
     switch(action.type){
         case GET_ITEMS:
             return{
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
             };
         case DELETE_ITEM:
             return{
@@ -26,6 +25,11 @@ export default function( state = initialState, action){
             return{
                 ...state,
                 items: [action.payload, ...state.items]
+            };
+        case ITEMS_LOADING:
+            return{
+                ...state,
+                loading: true
             };
         default:
             return state;
