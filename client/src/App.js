1fa939864207620fbcd8f80d107cae7496697cf5
Brian <brian.s2@gmail.com>
2019-10-22 23:00:33 -0500

    added add list item button and rendering list to react viewport

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import AppNavbar from "./components/appNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import ShoppingList from "./components/shoppingList"
 
 class App extends Component{
   render(){
     return(
       <div className="App">
       <AppNavbar />
+      <ShoppingList />
     </div>
     );
   }
