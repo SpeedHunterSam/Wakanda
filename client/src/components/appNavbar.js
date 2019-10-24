0906f64e3e349873db99aeeb9c506ca3d82e3693
Brian <brian.s2@gmail.com>
2019-10-24 14:52:07 -0500

    different info shown based on login state

--- client/src/components/appNavbar.js
+++ client/src/components/appNavbar.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 
 import {
   Collapse,
@@ -14,13 +14,20 @@ import {
 import RegisterModal from "./auth/RegisterModal";
 import Logout from "./auth/Logout";
 import LoginModal from "./auth/LoginModal";
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
 
 class AppNavbar extends Component {
 
     state ={
             isOpen:false
-        }
+        };
     
+    
+    static propTypes = {
+        auth: PropTypes.object.isRequired
+    };
+
 
 toggle = () => {
     this.setState({
@@ -29,37 +36,62 @@ toggle = () => {
 }
 
 render(){
+
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authLinks = (
+        <Fragment>
+            <NavItem>
+                <span className="navbar-text mr-3">
+                    <italics>{user ? `Hello ${user.name}` : ""}</italics>
+                </span>
+                <Logout />
+            </NavItem>
+        </Fragment>
+
+    );
+
+    const guestLinks = (
+        <Fragment>
+            <NavItem>
+                <LoginModal />
+            </NavItem>
+            <NavItem>
+                <RegisterModal />
+            </NavItem>
+        </Fragment>
+
+    );
+
+
+
     return(
         <div>
-        <Navbar color="dark" dark expand="sm" className="mb-5">
-        <Container>
-            <NavbarBrand href="/">Stealth Rally</NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-                <Nav className="ml-auto" navbar>
-                    <NavItem>
-                        <RegisterModal/>
-                        </NavItem>
-                        <NavLink href="https://github.com/SpeedHunterSam/Wakanda">
-                            Join Rally
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">Stealth Rally</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            {isAuthenticated ? authLinks : guestLinks}
+                            <NavLink href="https://github.com/SpeedHunterSam/Wakanda">
+                                Join Rally
                         </NavLink>
-                        <NavLink href="https://github.com/SpeedHunterSam/Wakanda">
-                            Create Rally
+                            <NavLink href="https://github.com/SpeedHunterSam/Wakanda">
+                                Create Rally
                         </NavLink>
-                    <NavItem>
-                        <LoginModal/>
-                    </NavItem>
-                    <NavItem>
-                        <Logout/>
-                    </NavItem> 
-                </Nav>
-            </Collapse>
-        </Container>
-        </Navbar>
-    </div>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
     );
 
 }
 }
 
-export default AppNavbar
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, null)(AppNavbar);
